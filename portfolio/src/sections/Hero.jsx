b6a9cc6c4d04facbbf1ba0cea3c0390e2b553af2
Yashwanth <yasw2875@colorado.edu>
2024-10-29 20:52:36 -0500

    fix: position of hero page items

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -4,66 +4,31 @@ import CanvasLoader from '../components/CanvasLoader';
 import { Suspense } from 'react';
 import { PerspectiveCamera } from '@react-three/drei';
 import HackerRoom from '../components/HackerRoom';
-import { Leva, useControls } from 'leva';
+import { Leva } from 'leva';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
-
-    const controls = useControls('HackerRoom', {
-        positionX: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        positionY: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        positionZ: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        rotationX: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        rotationY: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        rotationZ: {
-            valueOf: 2.5,
-            min: -10,
-            max: 10
-        },
-        scale: {
-            valueOf: 0.1,
-            min: -10,
-            max: 10
-        }
-    })
-
+    const isMobile = useMediaQuery({ maxWidth: 768 })
     return (
-        <section className='min-h-screen w-full flex flex-col relative'>
-            <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-                <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Yashwanth Swamy <span className='waving-hand'>👋🏼</span></p>
+        <section className='min-h-screen w-full flex flex-col lg:flex-row relative'>
+            <div className='w-full lg:w-1/2 flex flex-col sm:mt-36 mt-20 gap-3 justify-start '>
+                <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>
+                    Hi, I am Yashwanth Swamy <span className='waving-hand'>👋🏼</span>
+                </p>
                 <p className='hero_tag text-gray_gradient'>
                     Innovating Through Code and Creative Solutions
                 </p>
             </div>
 
-            <div className='w-full h-full absolute inset-0'>
+            <div className='w-full lg:w-1/2 h-full absolute inset-0 lg:inset-auto lg:top-0 lg:right-0 lg:bottom-0'>
                 <Leva />
                 <Canvas className='w-full h-full'>
                     <Suspense fallback={CanvasLoader}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                        <PerspectiveCamera makeDefault position={[2, 0, 30]} />
                         <HackerRoom
                             position={[2, -8, 2]}
-                            rotation={[0, -Math.PI, 0]}
-                            scale={0.1}
+                            rotation={isMobile ? [0, -Math.PI, 0] : [0, -10, 0]}
+                            scale={isMobile ? 0.07 : 0.1}
                         />
 
                         <ambientLight intensity={1} />
@@ -75,7 +40,7 @@ const Hero = () => {
                 </Canvas>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
