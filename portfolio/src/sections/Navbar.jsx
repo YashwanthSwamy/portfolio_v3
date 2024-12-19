74d1a8c05c10d498c001c5335470b7ba141a7a4d
Yashwanth <yasw2875@colorado.edu>
2024-12-19 13:22:39 -0600

    feat: logo update and remove unused images

--- portfolio/src/sections/Navbar.jsx
+++ portfolio/src/sections/Navbar.jsx
@@ -27,9 +27,10 @@ const NavBar = () => {
         <div className=' fixed top-0 left-0 right-0 z-50 bg-black/90 text-white'>
             <div className='max-w-7xl mx-auto'>
                 <div className='flex justify-between items-center py-5 mx-auto c-space'>
-                    <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
-                        Yash
-                    </a>
+                    <div className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+                        <img src="assets/icon.png" alt="logo" className='' />
+                    </div>
+
                     <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label='Toggle Menu'>
                         <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='w-6 h-6' />
                     </button>
